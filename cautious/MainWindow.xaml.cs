beeb3a826cb573153b6e49a21727d73c347d8036
JDPDO <jdpdo@outlook.de>
2016-03-05 20:52:06 +0100

    With new class for the map rendering. (Doesn't work)

--- cautious/MainWindow.xaml.cs
+++ cautious/MainWindow.xaml.cs
@@ -33,10 +33,5 @@ namespace cautious
         {
             InitializeComponent();
         }
-
-        private void InitializeComponent()
-        {
-            throw new NotImplementedException();
-        }
     }
 }
---
+++ cautious/OsmMapRender.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using OsmSharp.Osm.Data.Memory;
+using OsmSharp.UI.Map.Styles.MapCSS;
+using OsmSharp.Math.Geo.Projections;
+using OsmSharp.UI.Map;
+
+namespace cautious
+{
+    public class OsmMapRender
+    {
+        public OsmMapRender()
+        {
+            this.Projection = new WebMercator();
+        }
+
+        private static Stream stream = File.Open(@"osm\source\germany-latest.osm.pbf", FileMode.Open);
+        MemoryDataSource dataSource = MemoryDataSource.CreateFromPBFStream(stream);
+        MapCSSInterpreter interpreter = new MapCSSInterpreter("");
+        Map map = new Map(new WebMercator());
+        map.AddLayerOsm(dataSource, interpreter);
+    }  
+}
