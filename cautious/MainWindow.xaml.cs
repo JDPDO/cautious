1f72f32018e7ba09b10d6ad9c1feb96cbc441228
JDPDO <jdpdo@outlook.de>
2016-04-24 12:26:20 +0200

    New Design, cleaning up files and new menubar.

--- cautious/MainWindow.xaml.cs
+++ cautious/MainWindow.xaml.cs
@@ -1,37 +1,95 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Globalization;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using MapControl;
+using cautious.windows;
 
-//OsmSharp implemantation
-using OsmSharp.Math.Geo;
-using OsmSharp.Math.Geo.Projections;
-using OsmSharp.UI;
-using OsmSharp.UI.Animations;
-using OsmSharp.UI.Map;
-using OsmSharp.UI.Renderer;
-using OsmSharp.Units.Angle;
-
-namespace cautious
+namespace LocationToVisibilityConverter
 {
-    /// <summary>
-    /// Interaktionslogik für MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
+            //TileImageLoader.Cache = new MapControl.Caching.ImageFileCache(TileImageLoader.DefaultCacheName, TileImageLoader.DefaultCacheFolder);
+            //TileImageLoader.Cache = new MapControl.Caching.FileDbCache(TileImageLoader.DefaultCacheName, TileImageLoader.DefaultCacheFolder);
+            //BingMapsTileLayer.ApiKey = "...";
+
             InitializeComponent();
+        }
+
+        private void MapMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ClickCount == 2)
+            {
+                map.ZoomMap(e.GetPosition(map), Math.Floor(map.ZoomLevel + 1.5));
+                //map.TargetCenter = map.ViewportPointToLocation(e.GetPosition(map));
+            }
+        }
+
+        private void MapMouseRightButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ClickCount == 2)
+            {
+                map.ZoomMap(e.GetPosition(map), Math.Ceiling(map.ZoomLevel - 1.5));
+            }
+        }
+
+        private void MapMouseMove(object sender, MouseEventArgs e)
+        {
+            var location = map.ViewportPointToLocation(e.GetPosition(map));
+            var latitude = (int)Math.Round(location.Latitude * 60000d);
+            var longitude = (int)Math.Round(Location.NormalizeLongitude(location.Longitude) * 60000d);
+            var latHemisphere = 'N';
+            var lonHemisphere = 'E';
+
+            if (latitude < 0)
+            {
+                latitude = -latitude;
+                latHemisphere = 'S';
+            }
+
+            if (longitude < 0)
+            {
+                longitude = -longitude;
+                lonHemisphere = 'W';
+            }
+
+            mouseLocation.Text = string.Format(CultureInfo.InvariantCulture,
+                "{0}  {1:00} {2:00.000}\n{3} {4:000} {5:00.000}",
+                latHemisphere, latitude / 60000, (double)(latitude % 60000) / 1000d,
+                lonHemisphere, longitude / 60000, (double)(longitude % 60000) / 1000d);
+        }
+
+        private void MapMouseLeave(object sender, MouseEventArgs e)
+        {
+            mouseLocation.Text = string.Empty;
+        }
+
+        private void MapManipulationInertiaStarting(object sender, ManipulationInertiaStartingEventArgs e)
+        {
+            e.TranslationBehavior.DesiredDeceleration = 0.001;
+        }
+
+        private void MapItemTouchDown(object sender, TouchEventArgs e)
+        {
+            var mapItem = (MapItem)sender;
+            mapItem.IsSelected = !mapItem.IsSelected;
+            e.Handled = true;
+        }
+
+        private void Item_Quellen_Click(object sender, RoutedEventArgs e)
+        {
+            Quellen quellen = new Quellen();
+            quellen.Owner = Application.Current.MainWindow;
+            quellen.Show();
+        }
+
+        private void Item_Über_Click(object sender, RoutedEventArgs e)
+        {
+            Über über = new Über();
+            über.Owner = Application.Current.MainWindow;
+            über.Show();
         }
     }
 }
